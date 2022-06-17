64d3139050462304dc095a3ab937bf39412ef966
Shakur Imperiale <56418342+MetaphorX@users.noreply.github.com>
2022-06-17 02:03:29 +0100

    Add files via upload

---
+++ backend/schemas/project.js
@@ -0,0 +1,38 @@
+export default{
+    name: "project",
+    title: "Project",
+    type: "document",
+    fields:[
+        {
+            name: "title",
+            title: "Title",
+            type: "string"
+            
+        },
+        {
+            name: "pdfFile",
+            title: "PDF File",
+            type: "file",
+            options:{
+                accept: ".pdf"
+            },
+            validation: (Rule) => Rule.required()
+
+        },
+        {
+            name: "department",
+            title: "Department",
+            type: "string"
+        },
+        {
+            name: "supervisedBy",
+            title: "Supervised By",
+            type: "string"
+        },
+        {
+            name: "projectLink",
+            title: "Project Link",
+            type: "string"
+        },
+    ]
+}
---
+++ backend/schemas/supervisor.js
@@ -0,0 +1,17 @@
+export default {
+    name: "supervisor",
+    title: "Supervisor",
+    type: "document",
+    fields:[
+        {
+            name: "supervisorName",
+            title: "Supervisor Name",
+            type: "string"
+        },
+        {
+            name: "image",
+            title: "Image",
+            type: "string"
+        },
+    ]
+}
